8d6ac7d3796f512aea404d9e7a925fc5de6e681c
talerqa <hitri3003@gmail.com>
2023-04-01 17:03:22 +0300

    refactoring code

--- src/components/Profile/Post/Post.tsx
+++ src/components/Profile/Post/Post.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import avatar from '../../../img/avatar.png';
 import style from './Post.module.css'
 
-const Post = (props: any) => {
-  console.log(props.message)
+type PostPropsType = {
+  likesCount: number,
+  message: string,
+}
+
+const Post = (props: PostPropsType) => {
+  console.log(props.message);
   return (
     <div className={style.item}>
       <img className={style.avatar} src={avatar} alt=""/>
       <span>{props.message}</span>
       <div>Like</div>
-      <span>{props.countLike}</span>
+      <span>{props.likesCount}</span>
     </div>
   )
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,14 +3,13 @@ import style from './Profile.module.css'
 import Post from './Post/Post';
 import MyPost from './MyPost/MyPost';
 
-
-const Profile = (props:any) =>  {
+const Profile = (props: any) => {
   return (
     <div className={style.Profile}>
       <MyPost/>
-      <Post message='Hi how are you' countLike={1}/>
-      <Post message='Its my first project'countLike={2}/>
-      <Post message='Its my second project' countLike={3}/>
+      <Post message="Hi how are you" likesCount={1}/>
+      <Post message="Its my first project" likesCount={2}/>
+      <Post message="Its my second project" likesCount={3}/>
     </div>
   );
 }
