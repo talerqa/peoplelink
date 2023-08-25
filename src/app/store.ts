fa04b61f24e63ed5c2818b9719eb71e390d3475d
Aliaksei <97165727+talerqa@users.noreply.github.com>
2023-08-25 20:07:58 +0300

    Ref Update store

--- src/app/store.ts
+++ src/app/store.ts
@@ -22,7 +22,6 @@ export const store = createStore(rootReducer, applyMiddleware(thunk));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
-
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
